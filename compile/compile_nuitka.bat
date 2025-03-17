fbfb04bc260c699bb1861f286dd6002aa88cfe87
Zhang Anjun <admin@zhanganjun.com>
2025-03-17 20:24:08 +0800

    v1.4-hotfix

--- compile/compile_nuitka.bat
+++ compile/compile_nuitka.bat
@@ -1,2 +1,2 @@
-nuitka --standalone --onefile --msvc=latest --show-memory --show-progress --output-dir=nuitka --remove-output ..\calendar_generator.py
+python -m nuitka --standalone --onefile --msvc=latest --show-memory --show-progress --output-dir=nuitka --remove-output ..\calendar_generator.py
 pause
